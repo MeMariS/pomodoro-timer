e992b7d539c8ec32722ba9a28156096bf9ba4cdf
arseniypom <arpomazkov@bk.ru>
2025-05-15 21:54:21 +0800

    Added switch between work and rest

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
+const WORK_MINUTES = 0;
+const WORK_SECONDS = 10;
+const REST_MINUTES = 0;
+const REST_SECONDS = 5;
+
 let timer = null;
-let minutes = 25;
-let seconds = 0;
+let minutes = WORK_MINUTES;
+let seconds = WORK_SECONDS;
+let currentState = 'work';
 
-const minutesElement = document.getElementById("minutes");
-const secondsElement = document.getElementById("seconds");
-const startButton = document.getElementById("start");
-const pauseButton = document.getElementById("pause");
-const stopButton = document.getElementById("stop");
+const minutesElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('seconds');
+const startButton = document.getElementById('start');
+const pauseButton = document.getElementById('pause');
+const stopButton = document.getElementById('stop');
 
-startButton.addEventListener("click", startTimer);
-pauseButton.addEventListener("click", pauseTimer);
-stopButton.addEventListener("click", stopTimer);
+startButton.addEventListener('click', startTimer);
+pauseButton.addEventListener('click', pauseTimer);
+stopButton.addEventListener('click', stopTimer);
 
 function startTimer() {
   if (timer === null) {
@@ -36,7 +42,24 @@ function countDown() {
     if (minutes === 0) {
       clearInterval(timer);
       timer = null;
-      alert("Время вышло! Сделай перерыв.");
+
+      if (currentState === 'work') {
+        alert('Время вышло! Сделай перерыв.');
+      } else if (currentState === 'rest') {
+        alert('Перерыв окончен! Пора за работу.');
+      }
+
+      currentState = currentState === 'work' ? 'rest' : 'work';
+
+      if (currentState === 'work') {
+        minutes = WORK_MINUTES;
+        seconds = WORK_SECONDS;
+      } else if (currentState === 'rest') {
+        minutes = REST_MINUTES;
+        seconds = REST_SECONDS;
+      }
+
+      startTimer();
       return;
     } else {
       minutes--;
